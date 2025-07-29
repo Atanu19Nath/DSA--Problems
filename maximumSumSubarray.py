6a1ff4702fb9c708d9bce679b9978a4da57122a2
Deb Nath <atanu.debnath@dxc.com>
2025-07-30 01:55:38 +0530

    First commit

---
+++ maximumSumSubarray.py
@@ -0,0 +1,37 @@
+#User function Template for python3
+class Solution:
+    def maximumSumSubarray (self,arr,k):
+        # code here 
+        
+        i = 0
+        j = 0
+        size = len(arr)
+        sum = 0
+        
+        ans = float('-inf')
+        
+        while j < size :
+            
+            sum  = sum + arr[j]
+            
+            
+            if j - i + 1 < k :
+                
+                j += 1
+            
+            elif j - i + 1 == k :
+                
+                ans = max(ans, sum)
+                sum = sum - arr[i]
+                
+                i += 1
+                j += 1
+                
+        return ans
+    
+obj = Solution()
+
+arr = [100,200,300,400]
+k = 2
+
+print(obj.maximumSumSubarray(arr,k))

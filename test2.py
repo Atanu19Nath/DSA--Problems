3303e44cb8bf6e1c92937cb2d439bb713a75894c
Deb Nath <atanu.debnath@dxc.com>
2025-07-31 22:43:09 +0530

    add new

--- test2.py
+++ test2.py
@@ -1,26 +1,15 @@
-class Solution:
-    def intersectSize(self,a, b):
+from collections import Counter
 
-        a.sort()
-        b.sort()
+arr= [1,3,2,3,4]
 
-        i = 0
-        j = 0
-        count = 0
-        while i < len(a) and j < len(b):
 
-            if a[i] < b[j]:
-                i +=1
-            elif a[i] > b[j]:
-                j +=1
-            
-            elif a[i] == b[j]:
-                count +=1
-                i +=1
-                j +=1
+ans = Counter(arr)
 
-        print(count)
+print(ans)
 
-s = Solution ()
+for i in range(len(ans)):
 
-s.intersectSize([1,2,4,3,5,6],[3,4,5,6,7])
+    if ans[arr[i]] > 1:
+
+        print(arr[i])
+        break

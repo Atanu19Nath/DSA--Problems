2dbbb1b4d1e2e13b6f66c4d45934f2b424d4f4b6
Deb Nath <atanu.debnath@dxc.com>
2025-08-01 09:37:23 +0530

    add new

--- Intersection of Two arrays with Duplicate Elements.py
+++ Intersection of Two arrays with Duplicate Elements.py
@@ -1,21 +1,28 @@
+from collections import Counter
+
 class Solution:
     def intersect(self, a, b):
         # code here
+        
+        if len(a) == 0 or len(b) == 0:
+            
+            return []
 
         a.sort()
         b.sort()
+    
 
         i = 0
         j = 0
 
-        ans_list = []
+        ans_list = {}
 
         while i < len(a) and j < len(b):
 
             if a[i] == b[j]:
 
                 if a[i] not in ans_list:
-                    ans_list.append(a[i])
+                    ans_list[a[i]] = 1
                 
                 i += 1
                 j += 1

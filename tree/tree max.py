22361a119901da0e2a82af8983b754b10a7487af
Deb Nath <atanudebnath67@gmail.com>
2025-08-30 19:55:10 +0530

    add new

---
+++ tree/tree max.py
@@ -0,0 +1,74 @@
+class Node:
+
+    def __init__(self,data):
+
+        self.data = data
+        self.left = None
+        self.right = None
+
+def insert(root,value):
+
+    if root == None:
+
+        new_node = Node(value)  
+        return new_node
+        
+    elif value < root.data:
+
+        root.left = insert(root.left,value)
+
+    elif value > root.data:
+
+        root.right = insert(root.right,value)
+
+    return root
+
+def preorder(root):
+
+    if root == None:
+
+        return []
+
+    return [root.data] + preorder(root.left) + preorder(root.right)
+
+def inorder(root):
+
+    if root == None:
+
+        return []
+    
+    return inorder(root.left) + [root.data] + inorder(root.right)
+
+def postorder(root):
+
+    if root == None:
+
+        return []
+    
+    return postorder(root.left) + postorder(root.right) + [root.data]
+
+
+def treemax(root):
+
+    if root == None:
+
+        return float("-inf")
+
+    left_subtree = treemax(root.left)
+    right_subtree = treemax(root.right)
+
+    return max(root.data,left_subtree,right_subtree)
+
+
+values = [50, 30, 20, 40, 70, 60, 80] 
+
+root = None
+
+for value in values:
+
+    root = insert(root,value)
+
+print("preorder = ")
+print(preorder(root))
+
+print("Tree Max =", treemax(root))

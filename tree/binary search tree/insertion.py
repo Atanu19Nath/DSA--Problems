80815b5d49fc628e676065464fec1ca5682ebb9b
Deb Nath <atanudebnath67@gmail.com>
2025-08-30 22:04:50 +0530

    add new

---
+++ tree/binary search tree/insertion.py
@@ -0,0 +1,45 @@
+class Node:
+
+    def __init__(self,data):
+
+        self.data = data
+        self.left = None
+        self.right = None
+
+def insert(root,value):
+
+    if root == None:
+
+        new_node = (value)  
+        return new_node
+        
+    elif value < root.data:
+
+        root.left = insert(root.left,value)
+
+    elif value > root.data:
+
+        root.right = insert(root.right,value)
+
+    return root
+
+def preorder(root):
+
+    if root == None:
+
+        return []
+
+    return [root.data] + preorder(root.left) + preorder(root.right)
+
+
+values = [50, 30, 20, 40, 70, 60, 80] 
+
+root = None
+
+for value in values:
+
+    root = insert(root,value)
+
+print("preorder = ")
+print(preorder(root))
+
